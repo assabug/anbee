138659036f7f85a7dc250aa3897f17cfc3eb594f
anbee <anbee@hanmail.net>
2019-08-22 23:07:44 +0900

    Spring Quick Start Charpter1 Class4

--- study/java/spring/BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ study/java/spring/BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,8 +1,21 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
+	private Speaker speaker;
+	private int price;
+	
 	public SamsungTV() {
-		System.out.println("SamsungTV 객체 생성");
+		System.out.println("SamsungTV(1) 객체 생성");
+	}
+	
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker() 호출");
+		this.speaker = speaker;
+	}
+	
+	public void setPrice(int price) {
+		System.out.println("===> setPrice() 호출");
+		this.price = price;
 	}
 	
 	public void initMehod() {
@@ -14,7 +27,7 @@ public class SamsungTV implements TV {
 	}
 	
 	public void powerOn() {
-		System.out.println("SamsungTV---전원 켠다.");
+		System.out.println("SamsungTV---전원 켠다. (가격: " + price + ")");
 	}
 	
 	public void powerOff() {
@@ -22,10 +35,12 @@ public class SamsungTV implements TV {
 	}
 	
 	public void volumeUp() {
-		System.out.println("SamsungTV---소리 올린다.");
+		speaker.volumeUp();
+		//System.out.println("SamsungTV---소리 올린다.");
 	}
 	
 	public void volumeDown() {
-		System.out.println("SamsungTV---소리 내린다.");
+		speaker.volumeDown();
+		//System.out.println("SamsungTV---소리 내린다.");
 	}
 }
